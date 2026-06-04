64b3707f4263d5ea5b9b2aa3b375e8d16f20cd90
Nguyen Tuan Duong <duonggk05@gmail.com>
2026-06-04 15:21:26 +0700

    fix: add undirected parameter

--- src/gnn/data.py
+++ src/gnn/data.py
@@ -5,13 +5,16 @@ from torch_geometric.utils import to_undirected
 def load_and_preprocess_data(
     data_path: str = '../data/processed/pyg_data.pt',
     device: torch.device | None = None,
+    undirected: bool = True
 ) -> tuple:
     if device is None:
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print(f"Device: {device}")
 
     data = torch.load(data_path)
-    data.edge_index = to_undirected(data.edge_index)
+    
+    if undirected:
+        data.edge_index = to_undirected(data.edge_index)
 
     train_features = data.x[data.train_mask]
     train_mean = train_features.mean(dim=0)
@@ -22,3 +25,4 @@ def load_and_preprocess_data(
     data = data.to(device)
 
     return data, device
+
