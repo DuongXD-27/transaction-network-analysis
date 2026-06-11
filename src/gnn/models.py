8a3f0b3783d15d94a5d28408f3656c7e2902bcb1
Nguyen Tuan Duong <duonggk05@gmail.com>
2026-06-11 21:22:05 +0700

    feat: update GCN and GraphSAGE classes

--- src/gnn/models.py
+++ src/gnn/models.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn.functional as F
-from torch_geometric.nn import GATv2Conv
-
+from torch_geometric.nn import GATv2Conv, GCNConv, SAGEConv
 
 class GAT(torch.nn.Module):
     def __init__(self, in_channels, hidden_channels, out_channels, heads=4, dropout=0.5):
@@ -14,6 +13,34 @@ class GAT(torch.nn.Module):
         x = F.dropout(x, p=self.dropout, training=self.training)
         x = self.conv1(x, edge_index)
         x = F.elu(x)
+        x = F.dropout(x, p=self.dropout, training=self.training)
+        x = self.conv2(x, edge_index)
+        return x
+
+class GCN(torch.nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels, dropout=0.5):
+        super().__init__()
+        self.conv1 = GCNConv(in_channels, hidden_channels)
+        self.conv2 = GCNConv(hidden_channels, out_channels)
+        self.dropout = dropout
+
+    def forward(self, x, edge_index):
+        x = self.conv1(x, edge_index)
+        x = F.relu(x)
+        x = F.dropout(x, p=self.dropout, training=self.training)
+        x = self.conv2(x, edge_index)
+        return x
+
+class GraphSAGE(torch.nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels, aggregator_type='mean', dropout=0.5):
+        super().__init__()
+        self.conv1 = SAGEConv(in_channels, hidden_channels, aggr=aggregator_type)
+        self.conv2 = SAGEConv(hidden_channels, out_channels, aggr=aggregator_type)
+        self.dropout = dropout
+
+    def forward(self, x, edge_index):
+        x = self.conv1(x, edge_index)
+        x = F.relu(x)
         x = F.dropout(x, p=self.dropout, training=self.training)
         x = self.conv2(x, edge_index)
         return x
